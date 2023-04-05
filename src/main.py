4bc7d3f3916f7cd558b715b7f41e9802c824093f
XanderVerMac <vera.macias26.av@gmail.com>
2023-04-05 11:32:21 -0500

    we implement the logic of guessing the number

--- src/main.py
+++ src/main.py
@@ -11,7 +11,24 @@ user_name=input("enter your username: ")
 
 random_number=randint(minimum_number,maximum_number)
 
-print(random_number)
+#print(random_number)
 
 print(f"welcome {user_name} you have to guess a number from {minimum_number} to {maximum_number}")
+
+while number_of_attemps < 7:
+    print("guess the number :)")
+    user_number=int(input())
+
+    number_of_attemps += 1
+
+    if random_number==user_number:
+        print("you win")
+        break
+    
+    elif random_number < user_number:
+        print("the number is less")
+    elif random_number > user_number:
+        print("the number is greater")
+
+
 
